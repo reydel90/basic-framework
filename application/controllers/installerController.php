fc45232614911696a37f31198e3ca6818e0d28b9
administrador <admin@iMac-de-administrador.local>
2016-02-29 17:41:59 +0100

    test installer controller 3

--- application/controllers/installerController.php
+++ application/controllers/installerController.php
@@ -4,13 +4,27 @@ use core\classes\Input as Input;
 use core\classes\Config as Config;
 
 class installerController extends Controller{
+    protected $appname = '',
+              $username = '',
+              $password = '';
 
     public function index(){
         self::check();
         echo 'Hello.. installer for this app'; 
         if(Input::exists()){
-            echo Input::get('test');
+            if(!empty(Input::get('appname'))){
+                $this->username = Input::get('appname');
+            }
+            if(!empty(Input::get('username'))){
+                $this->username = Input::get('username');
+            }
+            if(!empty(Input::get('password'))){
+                $this->username = Input::get('password');
+            }            
         }
+        echo $this->appname  . '<br>';
+        echo $this->username . '<br>';
+        echo $this->password . '<br>';
         $this->view('installer/index'); 
     }
     public function check(){ // si esta instalada la app redirecciona a la pagina principal!!
--- application/views/installer/index.php
+++ application/views/installer/index.php
@@ -1,4 +1,6 @@
 <form action="" method="post">
-	<input type="text" name="test">introduce el texto :</input>
-	<input type="submit">enviar</input>
+	appname  : <input type="text" name="appname"></input>
+	username : <input type="text" name="username"></input>
+	password : <input type="password" name="password"></input>
+	<input type="submit"></input>
 </form>
