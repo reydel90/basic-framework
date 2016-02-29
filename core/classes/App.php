647f6762f2e05b08e0bbf7a6810c30fc9c72cab5
administrador <admin@iMac-de-administrador.local>
2016-02-29 14:48:57 +0100

    test global to app.php 10

--- core/classes/App.php
+++ core/classes/App.php
@@ -31,17 +31,15 @@ class App{
         //-------   FIND AND SET THE METHOD  ----------// 
         if($this->default){
             if(isset($url[1])){
-                if(method_exists($this->controller, $url[1])){
-                    $this->method = $url[1];
-                    unset($url[1]);                
-                }                      
+                $this->method = $url[1];
+                unset($url[1]);                                      
             }
         }else if(isset($url[2])){
-            if(method_exists($this->controller, $url[2])){
-                $this->method = $url[2];
-                unset($url[2]);                
-            } 
+            $this->method = $url[2];
+            unset($url[2]);                
         }
+        if(method_exists($this->controller, $this->method)){
+        }        
         //------- FIND AND SET THE PARAMS ----------// 
         $this->params = $url ? array_values($url) : [];
         call_user_func_array([$this->controller, $this->method], $this->params);   
