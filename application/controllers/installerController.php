b78e17323ce6b923ddfc669a9d1d3d30b3987b28
administrador <admin@iMac-de-administrador.local>
2016-02-29 17:23:51 +0100

    test installer controller 2

--- application/controllers/installerController.php
+++ application/controllers/installerController.php
@@ -1,5 +1,6 @@
 <?php 
 use core\classes\Controller as Controller;
+use core\classes\Input as Input;
 use core\classes\Config as Config;
 
 class installerController extends Controller{
@@ -7,6 +8,9 @@ class installerController extends Controller{
     public function index(){
         self::check();
         echo 'Hello.. installer for this app'; 
+        if(Input::exists()){
+            echo Input::get('test');
+        }
         $this->view('installer/index'); 
     }
     public function check(){ // si esta instalada la app redirecciona a la pagina principal!!
