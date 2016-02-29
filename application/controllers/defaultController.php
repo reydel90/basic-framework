3d49e3ab9b8910e43be99f4d4c23f3efda25d322
administrador <admin@iMac-de-administrador.local>
2016-02-29 13:11:33 +0100

    testing routing sysmen

--- application/controllers/defaultController.php
+++ application/controllers/defaultController.php
@@ -1,6 +1,6 @@
 <?php 
 use core\classes\Controller as Controller;
-use core\classes\Config as Config;
+//use core\classes\Config as Config;
 
 class defaultController extends Controller{
     public function index(){
--- application/init.php
+++ application/init.php
@@ -1,6 +1,7 @@
 <?php 
 use core\classes\App as App;
 use core\classes\Parse as Parse;
+use core\classes\Controller as Controller;
 
 require_once VENDOR . 'autoload.php';
 
